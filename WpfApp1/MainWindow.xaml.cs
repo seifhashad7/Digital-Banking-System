0be77837a99779110a01ad907614b04b08bb67d0
seifh <seifh@SEIF>
2026-04-11 18:32:51 +0200

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void addEmployee_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void editEmployee_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CreateAccountButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditAccountButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteAccountButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ClearAccountsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExportDataButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
