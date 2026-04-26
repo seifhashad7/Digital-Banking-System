63ace4c89d77f50c17b6288c0099652f5e84dd9f
seif.hashad <seifherooz@gmail.com>
2026-04-26 20:17:51 +0300

    [Digital Banking system]
    	- Changing project name and configurations.
    	- Fixing crashing issues.

--- DigitalBankingSystem/MainWindow.xaml.cs
+++ DigitalBankingSystem/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace DigitalBankingSystem
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
