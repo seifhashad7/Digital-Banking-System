d916e7eb8481d781d91e32f899e1e74765aea05a
seif.hashad <seifherooz@gmail.com>
2026-04-29 15:16:50 +0300

    [Digital Banking System]
    		- Implementing delete account functionality.

--- DigitalBankingSystem/DeleteEditAcc_UC.xaml.cs
+++ DigitalBankingSystem/DeleteEditAcc_UC.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -18,9 +19,57 @@ namespace DigitalBankingSystem
     /// </summary>
     public partial class DeleteEditAcc_UC : UserControl
     {
+        private int? accountId;
         public DeleteEditAcc_UC()
         {
             InitializeComponent();
+        }
+
+        private void bankAccTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(int.TryParse(bankAccTB.Text, out int bankAccId))
+            {
+                accountId = bankAccId;
+            }
+            else
+            {
+                accountId = null;
+            }
+        }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+            string connection = "server=localhost;user=deskUser;password=1234;database=banking_system";
+
+            using (var conn = new MySqlConnection(connection))
+            {
+                try
+                {
+                    conn.Open();
+
+                    string query = "delete from account where id = @accountId";
+
+                    using (var cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@accountId", accountId ?? (object)DBNull.Value);
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Account deleted successfully!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("No account found with this id!");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Database error: " + ex.Message);    
+                }
+            }
         }
     }
 }
